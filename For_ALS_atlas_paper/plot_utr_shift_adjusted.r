0985fa76c4e1d13527903406bd9a72f42b3b34ab
AidenSb <aiden.sababi@gmail.com>
2022-03-01 01:28:55 +0000

    just updating the rep after a while

---
+++ For_ALS_atlas_paper/plot_utr_shift_adjusted.r
@@ -0,0 +1,58 @@
+PlotUTRLengthShift_2 <- function(results.table,
+                                plot.title = "Global shift in 3'UTR length",
+                                do.ranksum.test = TRUE,
+                                return.plot = TRUE,
+                                do.plot = FALSE) {
+
+  locations.res.table.up <- subset(results.table, FC_direction == "Up")
+  pos.upreg <- apply(as.matrix(locations.res.table.up[, c("SiteLocation","NumSites")]), 1, 
+                function(x) {relative_location(x[1], x[2])})
+  
+  locations.res.table.down <- subset(results.table, FC_direction == "Down")
+  pos.downreg <- apply(as.matrix(locations.res.table.down[, c("SiteLocation","NumSites")]), 1, 
+                function(x) {relative_location(x[1], x[2])})
+  
+  if (do.ranksum.test) {
+    this.test <- wilcox.test(pos.upreg, pos.downreg)
+    print("Wilcoxon Rank-sum test comparing relative peak locations for up- vs down-regulated peaks:")
+    print(paste0("P-value = ", this.test$p.value)) 
+  }
+  
+  ggData <- data.frame(Peak_location = c(pos.upreg, pos.downreg),
+                       FC_direction = c(rep("Up", length(pos.upreg)), rep("Down", length(pos.downreg))))
+  ggData$FC_direction <- factor(ggData$FC_direction, levels = c("Up", "Down"))
+  
+  pl.density <- ggplot(ggData, aes(Peak_location, stat(count), fill = FC_direction)) + 
+    geom_density(alpha = 0.8) + ylab("") + theme_void(base_size = 18) + 
+    theme(axis.text = element_blank(), axis.title=element_blank(), axis.ticks = element_blank()) +
+    ggtitle(paste0(plot.title,"\n","P-value = ", this.test$p.value)) +
+    theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) +
+    scale_fill_brewer(palette="Set1")
+  
+  pl.histogram <- ggplot(ggData, aes(Peak_location, fill = FC_direction)) + 
+    geom_histogram(position = position_dodge(), colour = "black", binwidth = 0.1, alpha = 0.8) +
+    theme_classic(base_size = 18) + xlab("Relative peak location") + ylab("Peak count") +
+    scale_fill_brewer(palette="Set1") + theme(legend.position = "right") +
+    guides(fill = guide_legend(title = "Fold-change\ndirection", title.position = "top"))
+    
+  
+  ### Combine the plots together
+  pl.combined <- cowplot::plot_grid(pl.density, pl.histogram, ncol=1, 
+                                    rel_heights = c(0.3, 0.8), axis = "lr", align = "v")
+  
+  if (do.plot) {
+    plot(pl.combined)
+  }
+  
+  if (return.plot) {
+    return(pl.combined)
+  }
+}
+
+
+relative_location <- function(location, n) {
+  make_range <- function(x){(x-min(x))/(max(x)-min(x))}
+  relative.locations <- make_range( (1:n) / n )
+  this.location <- relative.locations[location]
+  return(this.location)
+}
--- Pathology_vs_control_AD_Exc.R
+++ Pathology_vs_control_AD_Exc.R
@@ -3,7 +3,7 @@ library(presto)
 library(ggplot2)
 library(msigdbr)
 library(foreach)
-library(doParallel)
+library(parallel)
 library(fgsea)
 library(Seurat)
 library(tidyverse)
@@ -13,7 +13,7 @@ setwd('/data/APAproject/post_qual/data/control_vs_pathology')
 
 
 ## read in the Exc_CTandAD peaks so
-Exc_peaks_AD_CT <- readRDS('rdata/Exc_peaks_CTandAD_merged.rds')
+Exc_peaks_AD_CT <- readRDS('rdata/Exc_peaks_CTandAD_so_final.rds')
 
 #lets get the combinations of celltype vs assigned subclusters
 table(Exc_peaks_AD_CT$subclusterAssignment)
@@ -22,50 +22,69 @@ ADsc <- c("SFG:Exc.s0","SFG:Exc.s2" ,"SFG:Exc.s1","SFG:Exc.s10","SFG:Exc.s8","SF
         "SFG:Exc.s7","SFG:Exc.s6","SFG:Exc.s3","SFG:Exc.s5" )
 CTsc <- c("L5/6","L2/3","L5/6-CC","Neu-NRGN-I","Neu-mat","L4","Neu-NRGN-II")
 
-
-results_root <- 'results/Exc_AD_vs_DimCT/'
-
+cell_cmb <- list()
 for (ct_sc in CTsc){
   for (ad_sc in ADsc){
-    cell1 = ct_sc
-    cell2 = ad_sc
-    sanity <- paste0(cell1, '--vs--' , cell2)
-    print(sanity)
-    p <- NULL
-    res.table <- NULL
-    res.table <- DetectUTRLengthShift(peaks.object = neurons_peaks_so, 
-                                      gtf_gr = gtf_gr,
-                                      gtf_TxDb = gtf_TxDb,
-                                      population.1 = cell1, 
-                                      population.2 = cell2)
-    
-    print("Global 3UTR shift analysis done and now doing APAusage analysis")
-    apa.res <- NULL
-    tryCatch({apa.res <- DetectAEU(peaks.object = neurons_peaks_so, 
-                                   gtf_gr = gtf_gr,
-                                   gtf_TxDb = gtf_TxDb,
-                                   do.MAPlot = F,
-                                   population.1 = cell1,
-                                   population.2 = cell2)
-    }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
-    
-    cell1 = gsub('/','-',cell1)
-    cell2 = gsub('/','-',cell2)
-    plotoutname <- paste0(results_root,cell1,"_vs_",cell2,'_3UTR_shift_plot.pdf')
-    tablename <- paste0(results_root,cell1,"_vs_",cell2,'_3UTR_shift_table.tsv')
-    APAoutename <- paste0(results_root,cell1,"_vs_",cell2,'_APAusage_table.tsv')
-    print(plotoutname)
-    tryCatch({
-      write.table(res.table, file=tablename, sep='\t')
-      p <- PlotUTRLengthShift(res.table)
-      print('second check point')
-      write.table(apa.res, file=APAoutename, sep='\t')}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
-    pdf(file=plotoutname, width = 8.27, height = 11.69, paper='A4r', onefile=T)
-    print(p)
-    dev.off()
+    tmp_list <- list(c(ct_sc,ad_sc))
+    cell_cmb <- append(cell_cmb,tmp_list)
   }
 }
-######
-##############################################################################################################
-#################################### Dim neurons vs Kapmann Neurons ###############
-# read in the Dimitery neurosns
+cell_cmb
+
+results_root <- 'results/Exc_AD_vs_DimCT/'
+reference.file <- '/home/aiden/data/refgenome/refdata-gex-GRCh38-2020-A/genes//genes.gtf'
+gtf_gr <- rtracklayer::import(reference.file)
+gtf_TxDb <- GenomicFeatures::makeTxDbFromGFF(reference.file, format="gtf")
+
+## edit the plot UTRshift script
+tst <- trace(PlotUTRLengthShift, edit=TRUE)
+
+Exc_peaks_AD_CT <- SetIdent(Exc_peaks_AD_CT, value = 'subclusterAssignment')
+
+
+
+
+run_utr_shifts <- function(x){
+  cell1 = x[[1]]
+  cell2 = x[[2]]
+  sanity <- paste0(cell1, '--vs--' , cell2)
+  print(sanity)
+  p <- NULL
+  res.table <- NULL
+  res.table <- DetectUTRLengthShift(peaks.object = Exc_peaks_AD_CT, 
+                                    gtf_gr = gtf_gr,
+                                    gtf_TxDb = gtf_TxDb,
+                                    population.1 = cell1, 
+                                    population.2 = cell2)
+  
+  print("Global 3UTR shift analysis done and now doing APAusage analysis")
+  apa.res <- NULL
+  tryCatch({apa.res <- DetectAEU(peaks.object = Exc_peaks_AD_CT, 
+                                 gtf_gr = gtf_gr,
+                                 gtf_TxDb = gtf_TxDb,
+                                 do.MAPlot = F,
+                                 population.1 = cell1,
+                                 population.2 = cell2)
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  
+  cell1 = gsub('/','-',cell1)
+  cell2 = gsub('/','-',cell2)
+  plotoutname <- paste0(results_root,cell1,"_vs_",cell2,'_3UTR_shift_plot.pdf')
+  tablename <- paste0(results_root,cell1,"_vs_",cell2,'_3UTR_shift_table.tsv')
+  APAoutename <- paste0(results_root,cell1,"_vs_",cell2,'_APAusage_table.tsv')
+  print(plotoutname)
+  tryCatch({
+    write.table(res.table, file=tablename, sep='\t')
+    p <- PlotUTRLengthShift(res.table)
+    print('second check point')
+    write.table(apa.res, file=APAoutename, sep='\t')}, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  pdf(file=plotoutname, width = 8.27, height = 11.69, paper='A4r', onefile=T)
+  print(p)
+  dev.off()
+}
+
+
+
+
+
+r <- mclapply(cell_cmb, run_utr_shifts, mc.cores = 24)  
